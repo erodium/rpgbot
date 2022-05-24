021a0e2bb04706807519e2812bf38e6d6fdb3c37
liam <ltvanion@umich.edu>
2022-05-23 23:21:48 -0400

    add help

--- rpgbot/main.py
+++ rpgbot/main.py
@@ -8,6 +8,7 @@ import configparser
 from setup_db import setup_db
 from pathlib import Path
 from random import randrange
+import requests
 
 
 def connect_to_db(config):
@@ -24,7 +25,16 @@ def get_greeting():
         total_lines = len(lines)
         linenum = randrange(total_lines)
         greeting = lines[linenum]
-    return(greeting)
+    return greeting
+
+
+def check_pathbuilder_json_uri(content):
+    parts = content.split()
+    uri = parts[1]
+    url = uri.split("?")[0]
+    if url == 'https://pathbuilder2e.com/json.php':
+        return uri
+    return False
 
 
 class RPGBot(discord.Client):
@@ -57,31 +67,40 @@ class RPGBot(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-
         ###
-        # Check for $meta commands
+        # Check for $action commands
         ###
         if message.content.startswith('$'):
             if message.content.startswith('$hello'):
                 greeting = get_greeting().replace("$USER$", message.author.display_name)
+                logger.info(f"Greeting: {greeting}")
                 await message.channel.send(greeting)
-            elif message.content.startswith('$register'):
-                print('register')
             elif message.content.startswith('$list'):
                 print('list')
-        ###
-        # Check for normal slash-type actions
-        ###
-        elif message.content.startswith('/'):
-            if message.content.startswith('/r') or message.content.startswith('/roll'):
+            elif message.content.startswith('$register'):
+                valid_uri = check_pathbuilder_json_uri(message.content)
+                if valid_uri:
+                    uid = message.author.id
+                    result = self.register_character(uid, valid_uri)
+
+                    resp = requests.get(valid_uri, headers={"User-Agent": self.config['DEFAULT']['user_agent']})
+                    resp.raise_for_status()
+                    char_json = resp.json()
+                    print(char_json)
+                print('register')
+            elif message.content.startswith('$r') or message.content.startswith('$roll'):
                 to_roll = message.content.split()[1]
                 result = d20.roll(to_roll)
                 await message.channel.send(result)
         ###
-        # Check for !action commands
+        # Check for !meta commands
         ###
         elif message.content.startswith('!'):
-            if message.content.startswith('!m') or message.content.startswith('!mode'):
+            if message.content.startswith('!help'):
+                with open('../assets/help.txt') as f:
+                    msg = f.read()
+                await message.author.send(msg)
+            elif message.content.startswith('!m') or message.content.startswith('!mode'):
                 parts = message.content.split()
                 if len(parts) > 1:
                     mode = parts[1]
@@ -97,6 +116,11 @@ class RPGBot(discord.Client):
             q = f"SELECT value FROM {self.config['db-tables']['gamemode']} WHERE id=1;"
             val = self.con.execute(q).fetchone()[0]
             return val
+
+    def register_character(self, uid, valid_uri):
+        print(f'using {valid_uri} to register character to user {uid}')
+        with self.con:
+            q = f""
 
 
 logger = logging.getLogger('discord')
