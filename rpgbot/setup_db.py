cd41c02c90abd0dd39a1044421b5f9f02a4ffa05
erodium <ltvanion@umich.edu>
2022-05-25 00:14:35 -0400

    added cols to database

--- rpgbot/setup_db.py
+++ rpgbot/setup_db.py
@@ -1,14 +1,14 @@
-import sqlite3 as sql
 import configparser
+import sqlite3 as sql
 
 
 def setup_db(config=None):
     if not config:
         config = configparser.ConfigParser()
         config.read('config.ini')
-    
+
     con = sql.connect(config['db']['filename'])
-    
+
     with con:
         con.execute(f"""
             CREATE TABLE {config['db-tables']['users']} (
@@ -18,10 +18,35 @@ def setup_db(config=None):
         """)
         con.execute(f"""
             CREATE TABLE {config['db-tables']['characters']} (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                id TEXT NOT NULL PRIMARY KEY,
                 owner TEXT,
                 name TEXT,
-                str INTEGER, dex INTEGER, con INTEGER, int INTEGER, wis INTEGER, cha INTEGER
+                raw_json TEXT,
+                class TEXT,
+                level INTEGER,
+                ancestry TEXT,
+                heritage TEXT,
+                background TEXT,
+                alignment TEXT,
+                gender TEXT,
+                age TEXT,
+                deity TEXT,
+                keyability TEXT,
+                languages TEXT,
+                ancestryhp INTEGER, classhp INTEGER, bonushp INTEGER, bonushpPerLevel INTEGER, 
+                speed INTEGER, speedBonus INTEGER,
+                str INTEGER, dex INTEGER, con INTEGER, int INTEGER, wis INTEGER, cha INTEGER,
+                classDC INTEGER, 
+                perception INTEGER, fortitude INTEGER, reflex INTEGER, will INTEGER, 
+                heavy INTEGER, medium INTEGER, light INTEGER, unarmored INTEGER, 
+                advanced INTEGER, martial INTEGER, simple INTEGER, unarmed INTEGER, 
+                castingArcane INTEGER, castingDivine INTEGER, castingOccult INTEGER, castingPrimal INTEGER, 
+                acrobatics INTEGER, arcana INTEGER, athletics INTEGER, crafting INTEGER, deception INTEGER, 
+                diplomacy INTEGER, intimidation INTEGER, medicine INTEGER, nature INTEGER, occultism INTEGER, 
+                performance INTEGER, religion INTEGER, society INTEGER, stealth INTEGER, survival INTEGER, 
+                thievery INTEGER, 
+                acProfBonus INTEGER, acAbilityBonus INTEGER, acItemBonus INTEGER, acTotal INTEGER,
+                active INTEGER
             );
         """)
         con.execute(f"""
@@ -31,7 +56,7 @@ def setup_db(config=None):
             );
         """)
         con.commit()
-    
+
     msg = f"INSERT INTO {config['db-tables']['users']} (id, name) values(?, ?)"
     data = [
         (165885524580630528, 'lowerlight'),
@@ -39,14 +64,14 @@ def setup_db(config=None):
     with con:
         con.executemany(msg, data)
         con.commit()
-    
+
     msg = f"""
-        INSERT INTO {config['db-tables']['characters']} (id, owner, name, str, dex, con, int, wis, cha) 
-        values(?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO {config['db-tables']['characters']} (id, owner, name, str, dex, con, int, wis, cha, active) 
+        values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
     data = [
-        (1, 165885524580630528, 'Balls', 8, 9, 10, 11, 12, 13),
-        (2, 165885524580630528, 'Spindle', 13, 12, 11, 10, 9, 8),
+        ("165885524580630528_Balls", 165885524580630528, 'Balls', 8, 9, 10, 11, 12, 13, 1),
+        ("165885524580630528_Spindle", 165885524580630528, 'Spindle', 13, 12, 11, 10, 9, 8, 0),
     ]
     with con:
         con.executemany(msg, data)
