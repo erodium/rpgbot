630a79a898226a673acd49002e12b97ced9d41e7
liam <ltvanion@umich.edu>
2022-07-12 19:27:34 -0400

    fix an oops

--- rpgbot/config.py
+++ rpgbot/config.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 ROOT_DIR = Path(__file__).parents[1]
 CONFIG_FILENAME = ROOT_DIR / "config.ini"
-DB_PATH = ROOT_DIR.parents[0]
+DB_PATH = ROOT_DIR / "data"
 ASSETS_PATH = ROOT_DIR / "assets"
-
